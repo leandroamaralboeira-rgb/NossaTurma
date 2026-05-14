52a6d231203f2f89bb5c4baeef91b1b6a4e0b60c
Malu200908 <maria.l.fagundes@aluno.senai.br>
2026-05-14 16:09:18 -0300

    Tela da Maria

---
+++ app/src/main/java/com/example/nossaturma/maria/Maria_Activity.kt
@@ -0,0 +1,21 @@
+package com.example.nossaturma.maria
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nossaturma.R
+
+class Maria_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_maria)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

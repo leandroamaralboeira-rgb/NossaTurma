4b04c626d688b88362a94d35c7945c675d080848
manuellaasilva10-commits <manuella.a.silva10@aluno.senai.br>
2026-05-15 16:42:40 -0300

    Envio dos arquivos

---
+++ app/src/main/java/com/example/nossaturma/manuella/ManuellaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.nossaturma.manuella
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nossaturma.R
+
+class ManuellaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_manuella)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
